1b097e1b5705c3a508e9ee4af6dc076ef96c1429
kimguppy <wonny2333@naver.com>
2025-01-18 18:25:15 +0900

    py

---
+++ work2.py
@@ -0,0 +1,10 @@
+#문제 1: 리스트에서 중복된 숫자 제거하기
+#주어진 리스트에서 중복된 숫자를 제거하고, 
+#중복되지 않은 숫자들만 포함된 새로운 리스트를 반환하는 함수를 작성하세요.
+
+def remove_duplicates(numbers):
+    return 
+
+# 테스트
+numbers = [1, 2, 2, 3, 4, 4, 5]
+print(remove_duplicates(numbers))  # [1, 2, 3, 4, 5]
---
+++ work3.py
@@ -0,0 +1,12 @@
+#문제 4: 숫자들의 합 구하기
+#주어진 숫자 목록에서 주어진 범위 내의 숫자들의 합을 구하는 함수를 작성하세요. 범위는 포함되어야 하며, 
+#시작 값과 끝 값을 포함한 합을 구해야 합니다.
+
+def sum_in_range(numbers, start, end):
+    return 
+
+# 테스트
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+start = 3
+end = 7
+print(sum_in_range(numbers, start, end))  # 25
